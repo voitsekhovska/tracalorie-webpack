cbc6a2cbaa5bae5d38fa3a9db59efb0af94e1dbd
ye.voitsekhovska <sunakojane6@gmail.com>
2026-02-17 20:11:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Useful resources
+
+This project was built as part of a practice section in the [Modern JavaScript From The Beginning 2.0 course](https://www.udemy.com/course/modern-javascript-from-the-beginning/) on Udemy. Credit goes to the course author.
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/repo-name/",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
